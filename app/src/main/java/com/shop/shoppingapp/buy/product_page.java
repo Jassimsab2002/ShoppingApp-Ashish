2ad3c2bbb2b49e120c6b2bb6cc1993f3a92b3a6c
Jassimsab2002 <jassimsabri2002@gmail.com>
2021-04-28 16:34:13 +0000

    Shopping App

---
+++ app/src/main/java/com/shop/shoppingapp/buy/product_page.java
@@ -0,0 +1,16 @@
+package com.shop.shoppingapp.buy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.shop.shoppingapp.R;
+
+public class product_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_page);
+    }
+}
